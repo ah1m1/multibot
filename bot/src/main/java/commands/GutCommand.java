54ab1cb98908b07e76aa82afb873eec8ec228e12
shinixsensei <crezoxplays@gmail.com>
2021-09-06 22:02:32 +0200

    refactored prefix into savefile

--- bot/src/main/java/commands/GutCommand.java
+++ bot/src/main/java/commands/GutCommand.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class GutCommand {
     public static void gut(GuildMessageReceivedEvent event, String message){
-        String args = message.replace("!gut ","");
+        String args = message.replace("gut ","");
         event.getChannel().sendMessage("Toll " + args).queue();
 
     }
--- bot/src/main/java/listeners/CommandHandler.java
+++ bot/src/main/java/listeners/CommandHandler.java
@@ -5,6 +5,7 @@ import commands.HSCommand;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+import tools.Storage;
 
 public class CommandHandler extends ListenerAdapter {
 
@@ -12,18 +13,22 @@ public class CommandHandler extends ListenerAdapter {
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
         String message = event.getMessage().getContentRaw();
 
-        if(message.startsWith("!hi")) {
-            event.getChannel().sendMessage("hello world").queue();
-        }
-        if(message.startsWith("!gut")) {
-            GutCommand.gut(event, message);
-        }
+        if(message.startsWith("prefix")) {
+            message = message.replaceFirst(Storage.getValue("prefix"), "");
+
+            if(message.startsWith("!hi")) {
+                event.getChannel().sendMessage("hello world").queue();
+            }
+            if(message.startsWith("!gut")) {
+                GutCommand.gut(event, message);
+            }
 
 
-        //HSCOMMAND
+            //HSCOMMAND
 
-        if(message.startsWith("!HS")){
-            HSCommand.hsCommand(event);
+            if(message.startsWith("!HS")){
+                HSCommand.hsCommand(event);
+            }
         }
     }
 }
